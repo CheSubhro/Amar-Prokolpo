c584c6ef5a65da853a5e93f7c04fba8095d12afd
CheSubhro <mitsubhra@gmail.com>
2026-07-06 20:24:12 +0530

    refactor: remove ChakraProvider and set up MUI CssBaseline for global styles

--- Admin Panel/Frontend/src/App.jsx
+++ Admin Panel/Frontend/src/App.jsx
@@ -1,23 +1,19 @@
 
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ErrorBoundary } from './components/common';
 import AdminLayout from './layouts/AdminLayout';
 
 function App() {
     return (
-        <ChakraProvider value={defaultSystem}>
-            <Router>
-                <ErrorBoundary>
-                    <AdminLayout>
-                        {/* <Home /> */}
-                        <h1>Welcome to CheSubhro's App</h1>
-                    </AdminLayout>
-                </ErrorBoundary> 
-            </Router>
-        </ChakraProvider>
-    )
+        <Router>
+            <ErrorBoundary>
+                <AdminLayout>
+                    <h1>Welcome to CheSubhro's App</h1>
+                </AdminLayout>
+            </ErrorBoundary> 
+        </Router>
+    );
 }
 
-export default App
+export default App;
--- Admin Panel/Frontend/src/main.jsx
+++ Admin Panel/Frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import App from './App.jsx'
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { CssBaseline } from '@mui/material';
+
+createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-      <App />
+    <CssBaseline />
+    <App />
   </Provider>
 );
-
