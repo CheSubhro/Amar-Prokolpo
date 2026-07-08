610249f4b0fb4c3603b79b5bdc173d84e93075b4
CheSubhro <mitsubhra@gmail.com>
2026-07-08 15:17:40 +0530

    feat: add profile dropdown menu to navbar with navigation to profile and change password

--- Admin Panel/Frontend/src/components/layout/Navbar/Navbar.jsx
+++ Admin Panel/Frontend/src/components/layout/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
 
-import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Box, Avatar } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Box, Avatar, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useNavigate } from 'react-router-dom';
 import { Tooltip } from '../../common/index'; 
 
 const Navbar = ({ toggleSidebar }) => {
+    
+    const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleMenuOpen = (event) => setAnchorEl(event.currentTarget);
+    const handleMenuClose = () => setAnchorEl(null);
+
+    const handleNavigate = (path) => {
+        navigate(path);
+        handleMenuClose();
+    };
+
     return (
         <AppBar position="sticky" sx={{ bgcolor: 'background.paper', color: 'text.primary', boxShadow: 1 }}>
             <Toolbar>
@@ -24,7 +37,23 @@ const Navbar = ({ toggleSidebar }) => {
                         </IconButton>
                     </Tooltip>
                     
-                    <Avatar sx={{ bgcolor: 'primary.main', width: 35, height: 35 }}>A</Avatar>
+                    {/* Avatar with Clickable Menu */}
+                    <Avatar 
+                        onClick={handleMenuOpen} 
+                        sx={{ bgcolor: 'primary.main', width: 35, height: 35, cursor: 'pointer' }}
+                    >
+                        A
+                    </Avatar>
+
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                    >
+                        <MenuItem onClick={() => handleNavigate('/profile')}>Profile</MenuItem>
+                        <MenuItem onClick={() => handleNavigate('/change-password')}>Change Password</MenuItem>
+                        <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+                    </Menu>
                 </Box>
             </Toolbar>
         </AppBar>
